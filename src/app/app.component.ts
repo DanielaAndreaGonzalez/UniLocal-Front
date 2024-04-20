010eefa9255612b5f7e42357021a68df9073dfd9
DanielaAndreaGonzalez <daniela.andrea.gonzalez@correounivalle.edu.co>
2024-04-19 21:27:13 -0500

    Se hacen ajustes en clase explicados por el profesor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RegistroUsuarioComponent],
+  imports: [RouterOutlet,RegistroUsuarioComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front-unilocal-proyecto-final';
-  footer = '© 2023 Unilocal. Todos los derechos reservados.';
+  footer = '© 2024 Unilocal. Todos los derechos reservados.';
+
+
+  public buscarNumero(lista: number[],numero: number): boolean{
+    /*let respuesta = false;
+
+    for(let num in lista){
+      if(lista[num] == numero){
+        respuesta = true;
+      }
+    }*/
+    return true;
+  }
+
+
+
+
 }
+
+
